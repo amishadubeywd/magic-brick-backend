0109dc5030ab2d732bcb955ff684d122c927afc1
Avi Tamrakar <abis39112@gmail.com>
2025-06-07 18:52:16 +0530

    api re- testing

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,11 +1,10 @@
 import express from "express"
-import { getUserProfile, Home, login, register } from "../controller/auth-controller.js";
+import { getUserProfile,  login, register } from "../controller/auth-controller.js";
 
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("/").get(Home);
 router.route("/profile").get(getUserProfile);
 
 
